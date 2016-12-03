50d83dbdbbd0aaa4057d74cc846b8d2f164f3d30
Sergey Petrov <petrov-84@mail.ru>
2016-12-03 15:42:31 +0300

    add finish task hm08-01

--- hm8/01/index.js
+++ hm8/01/index.js
@@ -2,44 +2,25 @@
 	
 	var arrdata = [];
 	var linkData = "https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json";
-
-
-	inputField.addEventListener("input",function(event){ // ввод данных в input
-			var value = event.target.value
-			var result = [];
-				result = filterdata(arrdata,value)
-			if (result.length>0) {
-				
-				/*Не могу понять как обновить DOM*/
-			}
-	})
-
+	var source = document.getElementById("template").innerHTML;
+    var container = document.getElementById("container");
+    var templateFn = Handlebars.compile(source);
 
 	loadJSON(linkData).then(function(result){
 		arrdata = JSON.parse(result)
 		//сортировка
 		arrdata.sort(function(a,b){return a.name > b.name ? 1 : -1})
-		
 
-		var source = document.getElementById("template").innerHTML;
-	
-		var container = document.getElementById("container");
-		var templateFn = Handlebars.compile(source);
-		var html = templateFn({listcity:arrdata});
-		
-		container.innerHTML = html;
-	});
-
-
-
-	
+		container.innerHTML = templateFn({listcity:arrdata});
+		return arrdata;
+	}).then(arrdata => {
+        inputField.addEventListener("input",function(event){ // ввод данных в input
+                var value = event.target.value
+                var result = filterdata(arrdata,value);
+                container.innerHTML = templateFn({listcity:result});
+        });
+    });
 })()
-
-function buildView(array){
-	
-	
-}
-
 
 function filterdata(array, searchval){
 	var result =[];
