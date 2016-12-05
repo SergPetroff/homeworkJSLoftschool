ce84c850075b81f64b34621ed2a9a69f9615c51d
Sergey Petrov <petrov-84@mail.ru>
2016-12-05 17:32:31 +0300

    first commit home task hm9

---
+++ hm9VK/index.js
@@ -0,0 +1,35 @@
+(function(){
+
+	getListFriends()
+})();
+
+function getListFriends(){
+	return new Promise(function(resolve,reject){
+		VK.init({
+			apiId: 5758326
+		});
+		VK.Auth.login(function(response){
+			if(response.error){
+				reject(new Error('Не удалось авторизоваться'));
+			}else{
+				resolve(response)
+			}
+		})
+	},2).then(function(){
+			var source = document.getElementById("lefttemplate").innerHTML;
+    		var lefttcontainer = document.getElementById("leftwraptemplate");
+    		var leftttemplateFn = Handlebars.compile(source);
+		return new	Promise(function(resolve,reject){
+			VK.api("users.get",{'name_case':'gen'},function(response){
+				var userdata = response.response[0];
+				
+			})
+			VK.api('friends.get',{'fields': 'bdate, photo_50'},function(response){
+				var listFriends = response.response
+				lefttcontainer.innerHTML = leftttemplateFn({listfriends:listFriends});
+			})
+		})
+	}).then(function(){
+		
+	})
+}
