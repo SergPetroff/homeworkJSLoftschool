266c7ee031b55016517068537c9b8e447f3aee70
Sergey Petrov <petrov-84@mail.ru>
2016-12-17 18:00:14 +0300

    add hm10

---
+++ hm10/index.js
@@ -0,0 +1,52 @@
+(function(){
+	
+	var constCalc = new calculator(100);
+
+	function mysqrt(){
+		return this * this
+	}
+	console.log(mysqrt.apply(constCalc.sum(1,2,3)))
+	console.log(mysqrt.apply(constCalc.dif(10,20)))
+	console.log(mysqrt.apply(constCalc.div(2,2)))
+	console.log(mysqrt.apply(constCalc.mul(2,2)))
+})()
+
+function calculator(firstNumber){
+
+	return {
+		sum:function(){
+			var result = 0
+			for (var i = 0; i < arguments.length; i++) {
+				result = result + arguments[i]
+			}
+
+			return result + firstNumber
+		},
+		dif:function(){
+			var result = 0
+			for (var i = 0; i < arguments.length; i++) {
+				result = result + arguments[i]
+			}
+
+			return firstNumber - result
+		},
+		div:function(){
+			var result = 0;
+			result =  firstNumber / arguments[0];
+			if (typeof arguments[1] === 'number') {
+				result = result / arguments[1];
+			}
+			
+
+			return result
+		},
+		mul:function(){
+			var result = 0;
+			result = firstNumber * arguments[0];
+			if (typeof arguments[1] === 'number') {
+				result = result * arguments[1];	
+			}
+			return result
+		}
+	}
+}
