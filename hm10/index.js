e0bfb303673c1368e64e165aaf21521a1e99a16d
Sergey Petrov <petrov-84@mail.ru>
2016-12-19 21:51:12 +0300

    corrected

--- hm10/index.js
+++ hm10/index.js
@@ -1,17 +1,105 @@
 (function(){
 	
-	var constCalc = new calculator(100);
-
-	function mysqrt(){
-		return this * this
+	function Calc (num){
+		this.result = num;
+	};
+	Calc.prototype.sum = function(){
+		return [].reduce.call(arguments, function(prev,current){
+			return prev += current;
+		}, this.result);
 	}
-	console.log(mysqrt.call(constCalc.sum(1,2,3)))
-	console.log(mysqrt.call(constCalc.dif(10,20)))
-	console.log(mysqrt.call(constCalc.div(2,2)))
-	console.log(mysqrt.call(constCalc.mul(2,2)))
+	Calc.prototype.dif = function(){
+		return [].reduce.call(arguments, function(prev,current){
+			return prev -= current;
+		}, this.result);
+	}
+	Calc.prototype.div = function(){
+		return [].reduce.call(arguments, function(prev,current){
+			return prev/current
+		}, this.result);
+	}
+	Calc.prototype.mul = function(){
+		return [].reduce.call(arguments, function(prev,current){
+			return prev * current;
+		}, this.result);
+	}
+
+
+
+	// Конструктор 
+	function constCalc (num) {
+	    // Вызов конструктора родителя
+		Calc.apply(this, arguments);
+	}
+
+	// Наследование
+	constCalc.prototype = Object.create(Calc.prototype);
+	constCalc.prototype.constructor = constCalc;
+
+	// Переопределение метода sum
+	constCalc.prototype.sum = function() {
+	   // Вызвать метод родителя, передав ему текущие аргументы
+	   var result = Calc.prototype.sum.apply(this, arguments);
+	   // Возводим в степень
+	   return Math.pow(result, 2);
+	}
+	// Переопределение метода dif
+	constCalc.prototype.dif = function() {
+	   // Вызвать метод родителя, передав ему текущие аргументы
+	   var result = Calc.prototype.dif.apply(this, arguments);
+	   // Возводим в степень
+	   return Math.pow(result, 2);
+	}
+
+	// Переопределение метода div
+	constCalc.prototype.div = function() {
+	   // Вызвать метод родителя, передав ему текущие аргументы
+	   var result = Calc.prototype.div.apply(this, arguments);
+	   // Возводим в степень
+	   console.log(result)
+	   return Math.pow(result, 2);
+	}
+
+	// Переопределение метода div
+	constCalc.prototype.mul = function() {
+	   // Вызвать метод родителя, передав ему текущие аргументы
+	   var result = Calc.prototype.mul.apply(this, arguments);
+	   // Возводим в степень
+	   console.log(result)
+	   return Math.pow(result, 2);
+	}
+
+
+	var sum = new Calc(100);
+	console.log(sum.sum(1,2,3));
+
+	var sum2 = new constCalc(100);
+	console.log(sum2.sum(1,2,3));
+
+	var dif = new Calc(100);
+	console.log(dif.dif(10,20));
+
+	var dif2 = new constCalc(100);
+	console.log(dif2.dif(10,20));
+
+	var div = new Calc(100);
+	console.log(div.div(2,2));
+
+	var div2 = new constCalc(100);
+	console.log(div2.div(2,2));
+
+	var mul = new Calc(100);
+	console.log(mul.mul(2,2));
+
+	var mul2 = new constCalc(100);
+	console.log(mul2.mul(2,2));
+
+
+
 })()
 
-function calculator(firstNumber){
+
+/*function calculator(firstNumber){
 
 	return {
 		sum:function(){
@@ -49,4 +137,4 @@ function calculator(firstNumber){
 			return result
 		}
 	}
-}
+}*/
